272a08b8f7f80ff34e007633247634cd3724709f
bonnerwiener <maechel.c@gmail.com>
2014-05-05 09:33:09 +0200

    txt Above Examples none

--- js/lexical_decision_keyboard_response.js
+++ js/lexical_decision_keyboard_response.js
@@ -201,7 +201,7 @@ document.getElementById("btnExamples").addEventListener("click",
             {
                 isExamples = false;
                 document.getElementById('txtInExamples').style.display = 'none';
-                document.getElementById('txtAboveExamples').style.display = 'inline';
+                document.getElementById('txtAboveExamples').style.display = 'none';
                 document.getElementById('frmExamples').style.display = 'inline';
             }
             else if ($('#example1Box').val().toUpperCase() == 'LION' &&
