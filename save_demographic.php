2917d8c07688e389b21d021c03d7d06b250d4b56
Jeronim Morina <plattenschieber@gmail.com>
2014-05-12 07:58:06 +0200

    update demographic

--- save_demographic.php
+++ save_demographic.php
@@ -1,16 +1,20 @@
 <?php
 //Receive userData from JavaScript
+$vpnumber = $_POST["vpnumber"];
 $birthday = $_POST["birthday"];
+$education = $_POST["education"];
+$student = $_POST["student"];
 $studyfield = $_POST["studyfield"];
 $country = $_POST["country"];
 $mothertongue = $_POST["mothertongue"];
 $germanyears = $_POST["germanyears"];
+$diagnostik = $_POST["diagnostik"];
 
 include 'connect.php';
 
 //Insert variables into database
-mysql_query("INSERT INTO user_data (birthday, studyfield, country, mothertongue, germanyears) 
-VALUES ('$birthday', '$studyfield', '$country', '$mothertongue', '$germanyears');");
+mysql_query("INSERT INTO user_data (vpnumber, birthday, education, student, studyfield, country, mothertongue, germanyears, diagnostik) 
+VALUES ('$vpnumber','$birthday', '$education', '$student', '$studyfield', '$country', '$mothertongue', '$germanyears', '$diagnostik');");
 echo mysql_insert_id();
 //Disconnect
 mysql_close($con);
