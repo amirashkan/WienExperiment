f9f09f8b3c4d2f259775136185d750747330114f
Jeronim Morina <plattenschieber@gmail.com>
2014-05-12 07:47:40 +0200

    database connector for user_data

--- save_data.php
+++ save_data.php
@@ -1,20 +1,21 @@
 <?php
 //Receive variables from JavaScript
-$paradigm = $_POST["paradigm"];
-$time = $_POST["time"];
-$subject = $_POST["subject"];
-$trial = $_POST["trial"];
-$stimuli = $_POST["stimuli"];
-$probe = $_POST["trial"];
-$code = $_POST["code"];
-$response = $_POST["response"];
-$rt = $_POST["rt"];
+$vpnumber = $_POST["vpnumber"];
+$trial = $_POST["trial_number"];
+$ex0 = $_POST["ex0"];
+$ex1 = $_POST["ex1"];
+$ex2 = $_POST["ex2"];
+$ex3 = $_POST["ex3"];
+$ex4 = $_POST["ex4"];
+$ex5 = $_POST["ex5"];
+$ex6 = $_POST["ex6"];
+$ex7 = $_POST["ex7"];
 
 include 'connect.php';
 
 //Insert variables into database
-mysql_query("INSERT INTO testexp (paradigm, time, subject, trial, probe, code, response, rt) 
-VALUES ('$paradigm', '$time', '$subject', '$trial', '$code', '$probe','$response', '$rt');");
+mysql_query("INSERT INTO user_data (vpnumber, choosentreatment) 
+VALUES ('$vpnumber', '$choosentreatment');");
 
 //Disconnect
 mysql_close($con);
